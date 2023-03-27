7e2ccc769eb9ce70a818468b1fe78e8770185f0a
Marco Perales <marcoantonioperales96@gmail.com>
2023-03-27 20:08:55 -0300

    Ejercicio1

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static int sumasTres(int n1, int n2, int n3){
+        return n1 + n2 + n3;
+    }
+    public static void main(String[] args) {
+
+        System.out.println("Suma: " + Main.sumasTres(2,5,8));
+
+
+    }
+}
